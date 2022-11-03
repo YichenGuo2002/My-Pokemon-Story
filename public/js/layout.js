ff98747141c1dba3b9a8edca8db1f548fd30a356
YichenGuo2002 <2793146847@qq.com>
2022-11-03 19:15:02 -0400

    Updated header css

--- public/js/layout.js
+++ public/js/layout.js
@@ -7,3 +7,12 @@ searchButton.onclick = () => {
     window.location = `detail?pId=${searchInput.value}`;
 }
 */
+
+searchInput.addEventListener('focus', (event) => {
+    searchButton.style = "box-shadow: 0 0 5px  rgba(56,106,187,1);";
+    // Cannot use reference css color searchButton.style = "box-shadow: 0 0 5px  var(--main-border-radius):";
+  });
+
+  searchInput.addEventListener('blur', (event) => {
+    searchButton.style = "box-shadow:none;";
+  });
