96b16ac19e72ed75f8571e976b93c90f0b8c421c
YichenGuo2002 <2793146847@qq.com>
2022-11-04 02:05:56 -0400

    Add animation to /index page

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,29 @@
 let homeButton = document.getElementById('homeButton');
-let header = document.getElementById('header');
+
 homeButton.onclick = () =>{
     window.location = `all`;
 }
-header.style = "position:absolute;"
+/*
+let imageLimit = 1000;
+let image
+let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
 
+function disableScroll() {
+    // Get the current page scroll position 
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            scrollLeft += scrollTop - imageLimit;
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
+
+if(scrollTop = &&){
+    disableScroll();
+}
+*/
 
